3e214cbb5a464c465925f78dfb1dd413d80e0d74
Dan Connolly <dconnolly@kumc.edu>
2011-11-17 15:23:07 -0600

    got a tumor Grade query to work! (#547)   - allow query by NAACCR dates by using start_date rather than tval_char   - concept schemes include : in deid check   - include tumor_registry_ontology in concept dimension

--- heron_load/naaccr_txform.sql
+++ heron_load/naaccr_txform.sql
@@ -5,6 +5,8 @@
 -- note mis-spelling of schema name: naacr
 select "Accession Number--Hosp" from naacr.extract where 1=0;
 
+alter session set NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI';
+
 whenever sqlerror continue; -- in case index is already there
 create index patient_id on naacr.extract (
   "Patient ID Number");
@@ -58,10 +60,13 @@ order by 1 desc
  *  <jack.london@KimmelCancerCenter.org> 8/18/2011 11:13 AM
  */
 create or replace view tumor_grade_site_histology as
-select * from (
+select d.*, nc.codedcrp from (
 select ne."Accession Number--Hosp"
      , ne."Sequence Number--Hospital"
      , 'TR|Grade:' || ne."Grade" as concept_cd
+     , 'Grade' as ItemName
+     , ne."Grade" as value
+
 from naacr.extract ne
 
 union all
@@ -69,6 +74,8 @@ union all
 select ne."Accession Number--Hosp"
      , ne."Sequence Number--Hospital"
      , 'TR|Site:' || ne."Primary Site" as concept_cd
+     , 'Primary Site' as ItemName
+     , ne."Primary Site" as value
 from naacr.extract ne
 
 
@@ -76,6 +83,8 @@ union all
 select ne."Accession Number--Hosp"
      , ne."Sequence Number--Hospital"
      , 'TR|S:' || ne."Primary Site" || '|H:' || ne."Morph--Type&Behav ICD-O-3" as concept_cd
+     , 'Morph--Type&Behav ICD-O-3' as ItemName
+     , ne."Morph--Type&Behav ICD-O-3" as value
 from naacr.extract ne
 
 /*
@@ -85,7 +94,9 @@ separate facts for , 'Histologic Type ICD-O-3'
 There don't seem to be codes on t_code.
 */
 
-)
+) d
+join naacr.t_item ni on ni."ItemName" = d.itemname
+left join naacr.t_code nc on nc.itemid = ni."ItemID" and nc.codenbr = d.value
 where "Accession Number--Hosp" is not null
 order by "Accession Number--Hosp", "Sequence Number--Hospital", concept_cd
 ;
@@ -155,23 +166,21 @@ order by 1 desc
 create or replace view tumor_demo_admin as
 select d."Accession Number--Hosp"
      , d."Sequence Number--Hospital"
+     , d.start_date
      , d.value
      , d.ItemNbr
      , d.ItemName
      , nc.codedcrp
-     , d.valtype_cd
-     , d.tval_char
      , 'NAACCR|' || d.ItemNbr || ':' || d.codenbr as concept_cd
      , d.codenbr
 from (
 select "Accession Number--Hosp"
      , "Sequence Number--Hospital"
+     , to_date(null) as start_date
      , ItemNbr
      , ItemName
      , value
      , value as codenbr
-     , '@' as valtype_cd
-     , '@' as tval_char
 from "NAACR"."EXTRACT_EAV"
 where ItemName in (
   'Race 1', 'Sex'
@@ -186,15 +195,15 @@ union all
 
 select "Accession Number--Hosp"
      , "Sequence Number--Hospital"
+     , to_date(case length(value)
+       when 8 then value
+       when 6 then value || '01'
+       when 4 then value || '0101' end, 'yyyymmdd') as start_date
      , ItemNbr
      , ItemName
      , value
      , null as codenbr
-     , 'D' as valtype_cd
-     , case length(value)
-       when 8 then substr(value, 1, 4) || '-' || substr(value, 5, 2) || '-' || substr(value, 7, 2)
-       when 6 then substr(value, 1, 4) || '-' || substr(value, 5, 2)
-       when 4 then value end as tval_char
+
 from "NAACR"."EXTRACT_EAV" ne
 where ItemName in (
   'Date of Birth'
@@ -202,7 +211,7 @@ where ItemName in (
 , 'Date of Inpatient Adm'
 , 'Date of Inpatient Disch'
   )
-
+and length(value) in (4, 6, 8)
 ) d
 join naacr.t_item ni on ni."ItemName" = d.itemname
 left join naacr.t_code nc on nc.itemid = ni."ItemID" and nc.codenbr = d.codenbr
@@ -258,25 +267,22 @@ select
   ne."Patient ID Number" as MRN,
   ne."Accession Number--Hosp" || '-' || ne."Sequence Number--Hospital" as encounter_ide,
   av.concept_cd,
+  av.ItemName,
+  av.codedcrp,
   '@' provider_id,
-  to_date(ne."Date of Diagnosis"
-               || case length(ne."Date of Diagnosis")
-                  when 8 then ''
-                  when 6 then '01'
-                  when 4 then '0101'
-                  end, 'yyyymmdd') as start_date,
+  case when av.start_date is not null then av.start_date
+  else to_date(case length(ne."Date of Diagnosis")
+               when 8 then ne."Date of Diagnosis"
+               when 6 then ne."Date of Diagnosis" || '01'
+               when 4 then ne."Date of Diagnosis" || '0101'
+               end, 'yyyymmdd') end as start_date,
   '@' modifier_cd,
-  av.valtype_cd,
-  av.tval_char,
+  '@' as valtype_cd,
+  '@' as tval_char,
   to_number(null) as nval_num,
   null as valueflag_cd,
   null as units_cd,
-  to_date(ne."Date of Diagnosis"
-               || case length(ne."Date of Diagnosis")
-                  when 8 then ''
-                  when 6 then '28'
-                  when 4 then '1231'
-                  end, 'yyyymmdd') as end_date,
+  av.start_date as end_date,
   '@' location_cd,
   to_date(null) as update_date
   /* to_date(ne."Date of Last Contact", 'yyyymmdd') as update_date
@@ -285,24 +291,26 @@ from naacr.extract ne
 join (
 select tgsh."Accession Number--Hosp"
      , tgsh."Sequence Number--Hospital"
+     , to_date(null) as start_date
      , tgsh.concept_cd
-     , '@' as valtype_cd
-     , '@' as tval_char
+     , tgsh.ItemName
+     , tgsh.codedcrp
 from tumor_grade_site_histology tgsh
 union all
 select tda."Accession Number--Hosp"
      , tda."Sequence Number--Hospital"
+     , tda.start_date
      , tda.concept_cd
-     , tda.valtype_cd
-     , tda.tval_char
+     , tda.ItemName
+     , tda.codedcrp
 from tumor_demo_admin tda
 
 ) av
  on ne."Accession Number--Hosp" = av."Accession Number--Hosp"
 and ne."Sequence Number--Hospital" = av."Sequence Number--Hospital"
+where length(ne."Date of Diagnosis") in (4, 6, 8)
 /* TODO: figure out what's up with the 42 records with no Date of Diagnosis */
-where ne."Date of Diagnosis" is not null
 ;
 
 -- eyeball it:
--- select * from tumor_reg_facts order by mrn, encounter_ide;
+-- select * from tumor_reg_facts order by start_date desc, mrn, encounter_ide;
