6deb602524d1794f414493277765dae678a63738
Dan Connolly <dconnolly@kumc.edu>
2012-01-19 09:11:32 -0600

    parse site hierarchy from seer site recode (#736)

--- tumor_reg/seer_recode.py
+++ tumor_reg/seer_recode.py
@@ -16,9 +16,33 @@ def main(argv):
 
 def seer_parse(fp):
     doc = etree.HTML(fp.read())
-    log.debug('root elt: %s', doc.tag)
+    t1 = doc.xpath('.//table')[0]
+    log.debug('rows in main table: %s', len(t1))
+
+    parents = []
+    prev_label = None
+    for site_row in t1.xpath('tr[td/@headers="site"]'):
+        indent_txt = site_row.xpath('td[1]/table/tr/td')[0].text
+        indent = len(indent_txt) if indent_txt else 0
+        label = site_row.xpath('td[1]/table/tr/td')[1].text.strip()
+
+        while parents and indent < parents[-1][0]:
+            parents.pop()
+        if indent > (parents[-1][0] if parents else 0):
+            parents.append((indent, prev_label))
+
+        log.debug('site: %s %s\n%s', '*' * indent, label, parents)
+
+        prev_label = label
+
     for tr in doc.xpath('.//table/tr'):
         log.debug('row of size: %s', len(tr))
+        log.debug('cell size: %s',
+                  [(td.attrib.get('colspan', 1),
+                    td.attrib.get('rowspan', 1))
+                   for td in tr])
+        log.debug('cell text: %s',
+                  [td.text for td in tr])
 
 
 if __name__ == '__main__':
