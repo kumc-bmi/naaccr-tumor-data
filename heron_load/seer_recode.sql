73fe025bba74d511a3020fdf5cd9542e76dcc0b6
Dan Connolly <dconnolly@kumc.edu>
2012-01-20 13:50:24 -0600

    add trivial item_name field to seer_recode_facts to match tumor_reg_facts

--- heron_load/seer_recode.sql
+++ heron_load/seer_recode.sql
@@ -16,7 +16,7 @@ select "Accession Number--Hosp" from naacr.extract where 1=0;
 create or replace view seer_recode_facts as
 select MRN
      , ne."Accession Number--Hosp" || '-' || ne."Sequence Number--Hospital" as encounter_ide
-     , 'SEER_SITE:' || recode concept_cd
+     , 'SEER_SITE:' || recode concept_cd, '@' item_name
      , '@' provider_id
      , start_date
      , '@' modifier_cd
