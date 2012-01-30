2757e5880e76509eef1697af7cd807b4b45c46f6
Dan Connolly <dconnolly@kumc.edu>
2012-01-30 12:04:31 -0600

    ensure @deid link for naaccr_txform.sql (#736)

--- heron_load/naaccr_txform.sql
+++ heron_load/naaccr_txform.sql
@@ -5,6 +5,8 @@
 -- note mis-spelling of schema name: naacr
 select "Accession Number--Hosp" from naacr.extract where 1=0;
 select "Accession Number--Hosp" from naacr.extract_eav where 1=0;
+-- check for deid link and curated data
+select name from seer_site_terms@deid where 1=0;
 
 -- check for metadata tables
 select * from naacr.t_item where 1=0;
