a8e79c238628beebe24561ff246d3edfb8e3d226
Justin Connolly <j189c736@kumc.edu>
2014-06-12 14:02:30 -0500

    - pretty print XML from (first) document, with entities expanded  - try lxml and a custom DTDResolver to handle \s in external entity references    - never mind; we did a search-and-replace using a text editor instead  - change usage so that files are unzipped in advance    because XML parser doesn't know to look in the zip file for external entities
    with Dan C.

--- heron_staging/tumor_reg/csterms.py
+++ heron_staging/tumor_reg/csterms.py
@@ -2,13 +2,18 @@
 especially ER/PR status for breast cancer.
 
 Usage::
+
+first, download and unzip 3_CSTables(HTMLandXML).zip
+
 ???@@TODO
 
 '''
 
 
-import zipfile
-import xml.etree
+import os
+
+# on windows, via conda/anaconda
+from lxml import etree
 
 
 class CS(object):
@@ -26,19 +31,51 @@ class CS(object):
     xml_format = '3_CS Tables (HTML and XML)/XML Format/'
 
 
-def h(thezip):
-    contents = thezip.namelist()
-    #print 'the zip file has this many doodads:', len(contents)
-
+def xml_items(contents):
     good = [name
           for name in contents
-          if name.startswith(CS.xml_format)]
+          if name.endswith('.xml')]
 
     return good
 
-# ZipFile objects
-mydownloads = 'C:\\Users\\j189c736\\Downloads\\'
-myzip = zipfile.ZipFile(mydownloads + CS.cs_tables)
-print 'CS zipfile', myzip
 
-print 'XML format files:', len(h(myzip))
+from StringIO import StringIO
+
+
+class DTDResolver(etree.Resolver):
+    '''
+    work around:
+    lxml.etree.XMLSyntaxError: Invalid URI: notapps\notappcsextevalnontnm.xml, line 14, column 73
+    '''
+    def resolve(self, url, id, context):
+        print("Resolving URL '%s'" % url)
+        return self.resolve_file(open(url), context)
+
+
+def xstuff(names):
+    os.chdir(CS.xml_format)
+
+    parser = etree.XMLParser(load_dtd=True)
+    #parser.resolvers.add(DTDResolver())
+    for name in names:
+        print "document:", name
+        text_stream = open(name)
+        #text = text_stream.read()
+        #fixed_text = text.replace('\\', '/')
+        #tmp = "tempfix.xml"
+        #out_stream = open(tmp, 'w')
+        #out_stream.write(fixed_text)
+        #out_stream.close()
+        tree = etree.parse(text_stream, parser)
+        root = tree.getroot()
+        print name, root.tag, root.attrib
+        print(etree.tostring(root, pretty_print=True))
+        break
+
+
+
+
+all_files = os.listdir(CS.xml_format)
+names = xml_items(all_files)
+print 'XML format files:', len(names)
+xstuff(names)
