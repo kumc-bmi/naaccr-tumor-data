e4ac62d3dfc3891becb9e94a4a560bef4a9fbacd
Dan Connolly <dconnolly@kumc.edu>
2014-06-12 16:26:05 -0500

    - use explicit rather than ambient authority for file access   - use logging rather than print statements   - fold xml_items into main()

--- heron_staging/tumor_reg/csterms.py
+++ heron_staging/tumor_reg/csterms.py
@@ -9,18 +9,23 @@ first, download and unzip 3_CSTables(HTMLandXML).zip
 
 '''
 
-
-import os
+import logging
 
 # on windows, via conda/anaconda
 from lxml import etree
 
+from lafile import osRd
 
-def main():
-    all_files = os.listdir(CS.xml_format)
-    names = xml_items(all_files)
-    print 'XML format files:', len(names)
-    xstuff(names)
+
+log = logging.getLogger(__name__)
+
+
+def main(rd):
+    xml_dir = rd / CS.xml_format
+    targets = [target for target in xml_dir.subRdFiles()
+               if target.path.endswith('.xml')]
+    log.info('XML format files: %d', len(targets))
+    xstuff(xml_dir, targets)
 
 
 class CS(object):
@@ -38,27 +43,40 @@ class CS(object):
     xml_format = '3_CS Tables (HTML and XML)/XML Format/'
 
 
-def xml_items(contents):
-    good = [name
-          for name in contents
-          if name.endswith('.xml')]
-
-    return good
-
-
-def xstuff(names):
-    os.chdir(CS.xml_format)
-
+def xstuff(xml_dir, targets):
     parser = etree.XMLParser(load_dtd=True)
-    for name in names:
-        print "document:", name
-        text_stream = open(name)
+    parser.resolvers.add(LAResolver(xml_dir))
+    for f in targets:
+        log.debug("document: %s", f)
+        text_stream = f.inChannel()
         tree = etree.parse(text_stream, parser)
         root = tree.getroot()
-        print name, root.tag, root.attrib
-        print(etree.tostring(root, pretty_print=True))
+        log.debug('%s %s %s', f, root.tag, root.attrib)
+        log.debug('%s', etree.tostring(root, pretty_print=True))
         break
 
 
+class LAResolver(etree.Resolver):
+    '''Resolve entity references in context of a :class:`lafile.Rd`.
+    '''
+    def __init__(self, rd):
+        def resolve(url, name, context):
+            log.debug('resolving: %s', url)
+            return self.resolve_file(rd.subRdFile(url).inChannel(), context)
+
+        self.resolve = resolve
+
+
 if __name__ == '__main__':
-    main()
+    def _configure_logging(level=logging.DEBUG):
+        logging.basicConfig(level=level)
+
+    def _trusted_main():
+        import os
+
+        rd = osRd(os.curdir, lambda n: open(n), os.path, os.listdir)
+
+        main(rd)
+
+    _configure_logging()
+    _trusted_main()
