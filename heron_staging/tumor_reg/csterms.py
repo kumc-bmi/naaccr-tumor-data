dab46b3f52f07aee3f55137973e05e9315a4f465
Justin Connolly <j189c736@kumc.edu>
2014-06-12 10:51:11 -0500

    clarify external design constraints from AJCC
    with help from Dan C.

--- heron_staging/tumor_reg/csterms.py
+++ heron_staging/tumor_reg/csterms.py
@@ -1,4 +1,5 @@
-'''ccterms -- do something cools @@TODO
+'''ccterms -- make i2b2 terms from Collaborative Staging tables
+especially ER/PR status for breast cancer.
 
 Usage::
 ???@@TODO
@@ -10,19 +11,34 @@ import zipfile
 import xml.etree
 
 
+class CS(object):
+    '''
+    reference:
+
+    Collaborative Stage Version 02.05
+    (c) Copyright 2014 American Joint Committee on Cancer.
+    https://cancerstaging.org/cstage/Pages/default.aspx
+    '''
+
+    # https://cancerstaging.org/cstage/software/Documents/3_CSTables(HTMLandXML).zip
+    cs_tables = '3_CSTables(HTMLandXML).zip'
+
+    xml_format = '3_CS Tables (HTML and XML)/XML Format/'
+
+
 def h(thezip):
     contents = thezip.namelist()
     #print 'the zip file has this many doodads:', len(contents)
 
-    xml_format = '3_CS Tables (HTML and XML)/XML Format/'
     good = [name
           for name in contents
-          if name.startswith(xml_format)]
+          if name.startswith(CS.xml_format)]
 
     return good
 
 # ZipFile objects
-myzip = zipfile.ZipFile('C:\\Users\\j189c736\\Downloads\\3_CSTables(HTMLandXML).zip')
+mydownloads = 'C:\\Users\\j189c736\\Downloads\\'
+myzip = zipfile.ZipFile(mydownloads + CS.cs_tables)
 print 'CS zipfile', myzip
 
 print 'XML format files:', len(h(myzip))
