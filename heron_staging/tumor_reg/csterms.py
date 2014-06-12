9c4c4f5915f6c93c496486e68270cd08c52de0f8
Justin Connolly <j189c736@kumc.edu>
2014-06-12 14:06:30 -0500

    prune dead DTDResolver code
    with Dan C.

--- heron_staging/tumor_reg/csterms.py
+++ heron_staging/tumor_reg/csterms.py
@@ -39,40 +39,18 @@ def xml_items(contents):
     return good
 
 
-from StringIO import StringIO
-
-
-class DTDResolver(etree.Resolver):
-    '''
-    work around:
-    lxml.etree.XMLSyntaxError: Invalid URI: notapps\notappcsextevalnontnm.xml, line 14, column 73
-    '''
-    def resolve(self, url, id, context):
-        print("Resolving URL '%s'" % url)
-        return self.resolve_file(open(url), context)
-
-
 def xstuff(names):
     os.chdir(CS.xml_format)
 
     parser = etree.XMLParser(load_dtd=True)
-    #parser.resolvers.add(DTDResolver())
     for name in names:
         print "document:", name
         text_stream = open(name)
-        #text = text_stream.read()
-        #fixed_text = text.replace('\\', '/')
-        #tmp = "tempfix.xml"
-        #out_stream = open(tmp, 'w')
-        #out_stream.write(fixed_text)
-        #out_stream.close()
         tree = etree.parse(text_stream, parser)
         root = tree.getroot()
         print name, root.tag, root.attrib
         print(etree.tostring(root, pretty_print=True))
         break
-
-
 
 
 all_files = os.listdir(CS.xml_format)
