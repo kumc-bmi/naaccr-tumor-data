32a2716e04a8fa8a3a26293ca37a3be749c6e820
Dan Connolly <dconnolly@kumc.edu>
2014-06-13 17:03:26 -0500

    refactor csterms for testability   - separate xstuff between XML parsing each file (each_doc) and parsing terms from a document (doc_terms)   - add test document
     ref Makefile for download/unzip details

--- heron_staging/tumor_reg/csterms.py
+++ heron_staging/tumor_reg/csterms.py
@@ -3,7 +3,7 @@ especially ER/PR status for breast cancer.
 
 Usage::
 
-first, download and unzip 3_CSTables(HTMLandXML).zip
+first, download and unzip 3_CSTables(HTMLandXML).zip; see Makefile for details.
 
 ???@@TODO
 
@@ -22,10 +22,9 @@ log = logging.getLogger(__name__)
 
 def main(rd):
     xml_dir = rd / CS.xml_format
-    targets = [target for target in xml_dir.subRdFiles()
-               if target.path.endswith('.xml')]
-    log.info('XML format files: %d', len(targets))
-    xstuff(xml_dir, targets)
+    for doc in each_document(xml_dir):
+        terms = list(doc_terms(doc))
+        log.debug('@@terms doc: %s %d', doc, len(terms))
 
 
 class CS(object):
@@ -43,37 +42,65 @@ class CS(object):
     xml_format = '3_CS Tables (HTML and XML)/XML Format/'
 
 
-def xstuff(xml_dir, targets):
+def each_document(xml_dir):
+    targets = [target for target in xml_dir.subRdFiles()
+               if target.path.endswith('.xml')]
+    log.info('XML format files: %d', len(targets))
+
     parser = etree.XMLParser(load_dtd=True)
     parser.resolvers.add(LAResolver(xml_dir))
+
     for f in targets:
+
+        # For now, let's just parse one file.
         if not f.path.endswith('Breast.xml'):  #@@
             continue
+
         log.debug("document: %s", f)
-        tree = etree.parse(f.inChannel(), parser)
-        root = tree.getroot()
-        log.debug("root tag: %s", root.tag)
-        title = root.xpath('schemahead/title')[0]
-        maintitle = title.xpath('maintitle/text()')
-        sitesummary = title.xpath('sitesummary/text()')
-        log.debug('title: %s summary: %s',
-                  maintitle, sitesummary)
-
-        for table in root.xpath('cstable'):
-            tablename = table.xpath('tablename')[0]
-            tabletitle = tablename.xpath('tabletitle/text()')
-            tablesubtitle = tablename.xpath('tablesubtitle//text()')
-            log.debug('tabletitle: %s tablesubtitle: %s',
-                      tabletitle, tablesubtitle)
-
-            for row in table.xpath('row'):
-                code = row.xpath('code/text()')[0]
-                descrip = row.xpath('descrip/text()')[0]
-                log.debug('code: %s descrip: %s',
-                          code, descrip)
+        doc = etree.parse(f.inChannel(), parser)
+        yield doc.getroot()
 
 
-        log.debug('%s', etree.tostring(root, pretty_print=True))
+
+def doc_terms(doc_elt):
+    '''Extract i2b2 terms from CS Tables XML document.
+
+    :param doc_elt: root element of XML document from CS Tables
+    :return: generator of terms in doc
+
+    >>> from pkg_resources import resource_string
+    >>> text = resource_string(__name__, 'cstable_ex.xml')
+
+    >>> doc = etree.fromstring(text)
+    >>> terms = list(doc_terms(doc))
+    >>> terms[:2]
+    ['@@TABLE THINGY', '@@ROW THINGY']
+
+    '''
+    log.debug("root tag: %s", doc_elt.tag)
+    log.debug('document: %s', etree.tostring(doc_elt, pretty_print=True))
+
+    title = doc_elt.xpath('schemahead/title')[0]
+    maintitle = title.xpath('maintitle/text()')
+    sitesummary = title.xpath('sitesummary/text()')
+    log.debug('title: %s summary: %s',
+              maintitle, sitesummary)
+
+    for table in doc_elt.xpath('cstable'):
+        tablename = table.xpath('tablename')[0]
+        tabletitle = tablename.xpath('tabletitle/text()')
+        tablesubtitle = tablename.xpath('tablesubtitle//text()')
+        log.debug('tabletitle: %s tablesubtitle: %s',
+                  tabletitle, tablesubtitle)
+
+        yield '@@TABLE THINGY'
+
+        for row in table.xpath('row'):
+            code = row.xpath('code/text()')[0]
+            descrip = row.xpath('descrip/text()')[0]
+            log.debug('code: %s descrip: %s',
+                      code, descrip)
+            yield '@@ROW THINGY'
 
 
 class LAResolver(etree.Resolver):
@@ -81,7 +108,7 @@ class LAResolver(etree.Resolver):
     '''
     def __init__(self, rd):
         def resolve(url, name, context):
-            log.debug('resolving: %s', url)
+            #log.debug('resolving: %s', url)
             return self.resolve_file(rd.subRdFile(url).inChannel(), context)
 
         self.resolve = resolve
