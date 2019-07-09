56981ca17c5424241b4292eba27919571a8fbad9
Dan Connolly <dconnolly@kumc.edu>
2019-07-09 18:11:14 -0500

    csvdb: load Loinc_2.66.zip into sqlite3 db

--- naaccr_ddict/csvdb.py
+++ naaccr_ddict/csvdb.py
@@ -5,15 +5,20 @@ Usage:
 or
   python csvdb.py --load csv_dir_name db_filename
 or
+  python csvdb.py --initz zip_file_name db_filename
+or
   python csvdb.py --loadz zip_file_name db_filename
 or
   python csvdb.py --queryz zip_file_name sql_query
 
-Design note: this is a single-file script with no dependencies outside the python stdlib.
+Design note: this is a single-file script with no dependencies
+outside the python stdlib.
 
 '''
 import csv
-from posixpath import join as posix_join, splitext
+import fnmatch
+from io import TextIOWrapper
+from posixpath import join as posix_join, basename, splitext
 from zipfile import ZipFile
 import logging
 
@@ -39,6 +44,8 @@ def main(argv, stdout, stderr, cwd, basename, connect):
         dump_db(db, csv_storage)
     elif flag == '--load':
         load_db(db, csv_storage)
+    elif flag == '--initz':
+        initz_db(db, csv_storage)
     elif flag == '--loadz':
         loadz_db(db, csv_storage, basename(csv_dir_name))
     elif flag == '--queryz':
@@ -66,19 +73,37 @@ def run_query(db, query, out):
     sep = ['-' * w for (_, w) in widths]
     fmt = '  '.join('{{{ix}:{w}}}'.format(w=w, ix=ix)
                     for (ix, (_, w)) in enumerate(widths))
-    fmt_record = lambda row: fmt.format(*row)
+    fmt_record = lambda row: fmt.format(*row)  # noqa
     print(fmt_record(header), file=out)
     print(fmt_record(sep), file=out)
     for row in rows:
         print(fmt_record(row), file=out)
 
 
+def initz_db(db, csv_storage):
+    zstore = zip_path(csv_storage)
+    work = db.cursor()
+    for data in zstore.glob('*/*.csv'):
+        name = data.stem
+        fields = next(csv.reader(data.open()))
+        col_specs = ', '.join(f'"{col}" text' for col in fields)
+        ddl = f'create table "{name}" ({col_specs})'
+        log.info('create %s for %s', name, data)
+        work.execute(f'drop table if exists "{name}"')
+        work.execute(ddl)
+
+
 def loadz_db(db, csv_storage, csv_dir_name):
-    name, _ext = splitext(csv_dir_name)
-    load_db(db, zip_path(csv_storage) / name)
+    zstore = zip_path(csv_storage)
+    to_path = {data.stem: data
+               for data in zstore.glob('*/*.csv')}
+    load_db(db, zip_path(csv_storage),
+            find_data=lambda t: to_path[t])
 
 
-def load_db(dest, src, setup=None):
+def load_db(dest, src,
+            setup=None,
+            find_data=None):
     '''Initialize DB and for each table, load data from CSV file, if available.
 
     :param Connection dest:
@@ -88,14 +113,16 @@ def load_db(dest, src, setup=None):
     '''
     if setup is None:
         setup = [(src / 'schema.sql')]
+    if find_data is None:
+        find_data = lambda t: src / (t.lower() + '.csv')  # noqa
 
     def get_rows(table):
-        data = src / (table.lower() + '.csv')
+        data = find_data(table)
         if data.exists():
             rows = csv.reader(data.open())
             return next(rows), rows
         else:
-            log.debug('skipping %: no data file', table)
+            log.debug('skipping %s: no data file', table)
             return [], []
     load_tables(dest, get_rows, setup)
 
@@ -108,8 +135,9 @@ def load_tables(dest, get_rows, setup):
     :type setup: list[Path]
     '''
     for script in setup:
-        log.info('running setup script: %s', script)
-        dest.executescript(script.open(mode='r').read())
+        if script.exists():
+            log.info('running setup script: %s', script)
+            dest.executescript(script.open(mode='r').read())
 
     for table in db_tables(dest):
         header, rows = get_rows(table)
@@ -141,21 +169,20 @@ def load_table(dest, table, header, rows,
     :type batch_size: int
     :type rows: iter[list[str]]
     '''
-    log.debug('loading %s', table)
+    log.info('loading %s', table)
     work = dest.cursor()
     stmt = """
-      insert into {table} ({header})
+      insert into "{table}" ({header})
       values ({placeholders})""".format(
           table=table,
-          header=', '.join(header),
+          header=', '.join(f'"{col}"' for col in header),
           placeholders=', '.join('?' for _ in header))
     log.debug('%s', stmt)
     batch = []
 
     def do_batch():
         work.executemany(stmt, batch)
-        log.info('inserted %s rows into %s',
-            len(batch), table)
+        log.info('inserted %s rows into %s', len(batch), table)
         del batch[:]
 
     for row in rows:
@@ -201,11 +228,19 @@ class Path(object):
         '''
         :param str here:
         '''
-        io_open, path_join, path_exists = ops
-        self.joinpath = lambda there: Path(path_join(here, there), ops)
+        io_open, path_join, path_exists, glob = ops
+        make = lambda there: Path(there, ops)  # noqa
+        self.joinpath = lambda there: make(path_join(here, there))
         self.open = lambda **kwargs: io_open(here, **kwargs)
         self.exists = lambda: path_exists(here)
+        self.glob = lambda pattern: (make(it)
+                                     for it in glob(path_join(here, pattern)))
         self._path = here
+
+    @property
+    def stem(self):
+        s, _ = splitext(self._path)
+        return basename(s)
 
     def __repr__(self):
         return '{cls}(...)'.format(cls=self.__class__.__name__)
@@ -221,8 +256,11 @@ class Path(object):
         return self.joinpath(there)
 
 
-def zip_path(backing_store):
+def zip_path(backing_store,
+             encoding='ISO-8859-1'):
     """Adapt Zipfile to pathlib API
+
+    Ugh! LoincIeeeMedicalDeviceCodeMappingTable.csv is ISO8859-1
 
     :type backing_store: Path
     """
@@ -230,11 +268,16 @@ def zip_path(backing_store):
 
     def zip_open(there, mode='r'):
         mode = mode[:1]  # zipfile supports r but not rb
-        return zbak.open(there, mode=mode)
+        return TextIOWrapper(zbak.open(there, mode=mode),
+                             encoding=encoding)
 
     def path_exists(there):
         return there in zbak.namelist()
-    return Path('', (zip_open, posix_join, path_exists))
+
+    def glob(pat):
+        return fnmatch.filter(zbak.namelist(), pat)
+
+    return Path('', (zip_open, posix_join, path_exists, glob))
 
 
 class Connection(object):
@@ -292,13 +335,14 @@ class Cursor(object):
 
 if __name__ == '__main__':
     def _privileged_main():
+        from glob import glob
         from io import open as io_open
         import os.path as os_path
         from sqlite3 import connect
         from sys import argv, stdout, stderr
 
         main(argv, stdout, stderr,
-             cwd=Path('.', (io_open, os_path.join, os_path.exists)),
+             cwd=Path('.', (io_open, os_path.join, os_path.exists, glob)),
              basename=os_path.basename,
              connect=lambda path: connect(str(path)))
 
