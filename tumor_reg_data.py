610ec58bb79431cebd0f73155397a6ff164d0bfd
Dan Connolly <dconnolly@kumc.edu>
2019-08-19 16:30:41 -0500

    tumor_reg_data: prune notebook metadata from .py

--- tumor_reg_data.py
+++ tumor_reg_data.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:hydrogen
-#     text_representation:
-#       extension: .py
-#       format_name: hydrogen
-#       format_version: '1.2'
-#       jupytext_version: 1.2.1
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
 # %% [markdown]
 # # NAACCR Tumor Registry Data
 #
