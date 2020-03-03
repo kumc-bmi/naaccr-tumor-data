086132f21a7cd8df3956c72f841865a6928c8bb8
Dan Connolly <dconnolly@kumc.edu>
2020-03-03 17:16:16 -0600

    NAACCR_I2B2.ont_view_in() with test (WIP)
      - switch OncologyMeta.read_table() back to Tuple for type safety;
        comes with brevity benefit
      - use @CompileStatic for TumorOnt
      - build tablesaw Table from list of groovy maps (at a cost of type
        safety)

--- src/main/groovy/TumorOnt.groovy
+++ src/main/groovy/TumorOnt.groovy
@@ -1,18 +1,22 @@
-import tech.tablesaw.api.Table
+import groovy.transform.CompileStatic
+import tech.tablesaw.api.*
+import tech.tablesaw.columns.Column
 import tech.tablesaw.io.csv.CsvReadOptions
 
-import javax.annotation.Nullable
 import java.nio.charset.Charset
 import java.nio.file.Path
+import java.time.LocalDate
 import java.util.zip.ZipFile
 
+@CompileStatic
 class TumorOnt {
     static class OncologyMeta {
-        static def morph3_info = [zip:'ICD-O-2_CSV.zip', item: 'icd-o-3-morph.csv', names: ['code', 'label', 'notes']]
-        static def topo_info = [zip: 'ICD-O-2_CSV.zip', item: 'Topoenglish.txt', names: null]
-        static def encoding = 'ISO-8859-1'
+        static Tuple morph3_info = new Tuple('ICD-O-2_CSV.zip', 'icd-o-3-morph.csv', ['code', 'label', 'notes'])
+        static Tuple topo_info = new Tuple('ICD-O-2_CSV.zip', 'Topoenglish.txt', null)
+        static String encoding = 'ISO-8859-1'
 
-        static Table read_table(Path cache, String zip, String item, @Nullable List<String> names) {
+        static Table read_table(Path cache, Tuple info) {
+            String zip = info[0] as String; String item = info[1] as String; List<String> names = info[2] as List<String>
             def archive = new ZipFile(cache.resolve(zip).toFile())
             def infp = new InputStreamReader(archive.getInputStream(archive.getEntry(item)), Charset.forName(encoding))
             CsvReadOptions options = CsvReadOptions.builder(infp)
@@ -21,7 +25,7 @@ class TumorOnt {
                     .build()
             Table data = Table.read().csv(options)
             if (names) {
-                listZip(names, data.columns()) { name, col -> col.setName(name) }
+                listZip(names, data.columns()) { String name, Column<?> col -> col.setName(name) }
             }
             data
         }
@@ -31,5 +35,63 @@ class TumorOnt {
         def result = []
         0.upto(Math.min(a.size(), b.size()) - 1) { ix -> result << f(a[ix], b[ix]) }
         result
+    }
+
+    static String top_folder = "\\i2b2\\naaccr\\"
+    static String c_name = 'Cancer Cases (NAACCR Hierarchy)'
+    static String sourcesystem_cd = 'heron-admin@kumc.edu'
+    static class NAACCR_I2B2 {
+        static def ont_view_in(String task_hash, LocalDate update_date, Path who_cache) {
+            // TODO: make who_cache optional
+            def info = OncologyMeta.topo_info
+            def who_topo = OncologyMeta.read_table(who_cache, info)
+            // TODO info = OncologyMeta.icd_o_info
+            // def icd_o_topo = OncologyMeta.read_table(who_cache, info.zip, info.items, info.names)
+            Table top = build([[c_hlevel :1,
+                                c_fullname: top_folder,
+                                c_name: c_name,
+                                update_date: update_date,
+                                sourcesystem_cd: sourcesystem_cd] as Map])
+            def current_task = build([[task_hash: task_hash] as Map]).setName("current_task")
+            [who_topo, top, current_task]
+        }
+    }
+
+    static Table build(List<Map<String, Object>> obj) {
+        Collection<Column<?>> cols = (obj[0].collect { k, v ->
+            switch(v) {
+                case String:
+                    return StringColumn.create(k, v as String)
+                case Integer:
+                    return IntColumn.create(k, v as Integer)
+                case LocalDate:
+                    return DateColumn.create(k, v as LocalDate)
+                default:
+                    throw new IllegalArgumentException("Expected String or Int in 1st record, not:" + v)
+            }
+        }) as Collection<Column<?>>
+        Table data = Table.create("t1", cols)
+        obj.tail().each { Map<String, Object> m ->
+            Row row = data.appendRow()
+            for (it in m) {
+                switch(it.value) {
+                    case String:
+                        row.setString(it.key, it.value as String)
+                        break
+                    case Integer:
+                        row.setInt(it.key, it.value as Integer)
+                        break
+                    case LocalDate:
+                        row.setDate(it.key, it.value as LocalDate)
+                        break
+                    case null:
+                        row.setMissing(it.key)
+                        break
+                    default:
+                        throw new IllegalArgumentException("not supported" + it.value)
+                }
+            }
+        }
+        data
     }
 }
--- src/test/groovy/TumorOntTest.groovy
+++ src/test/groovy/TumorOntTest.groovy
@@ -1,13 +1,14 @@
 import junit.framework.TestCase
 
 import java.nio.file.Paths
+import java.time.LocalDate
 
 class TumorOntTest extends TestCase  {
     static def cache = ',cache/'
 
     void testOncologyMeta() {
         def info = TumorOnt.OncologyMeta.morph3_info
-        def morph = TumorOnt.OncologyMeta.read_table(Paths.get(cache), info.zip, info.item, info.names)
+        def morph = TumorOnt.OncologyMeta.read_table(Paths.get(cache), info)
         assert morph.columnNames() == ["code", "label", "notes"]
 
         // test encoding
@@ -16,8 +17,26 @@ class TumorOntTest extends TestCase  {
         assert non_ascii.get(0, 1) == "M\u009Fllerian mixed tumour"
 
         info = TumorOnt.OncologyMeta.topo_info
-        def topo = TumorOnt.OncologyMeta.read_table(Paths.get(cache), info.zip, info.item, info.names)
+        def topo = TumorOnt.OncologyMeta.read_table(Paths.get(cache), info)
         println(topo.first(5))
         assert topo.columnNames() == ["Kode", "Lvl", "Title"]
+    }
+
+
+    void testTableExpr() {
+        def obj = [
+                [c_facttablecolumn: "x", c_comment: "xx", c_totalnum: 1],
+                [c_facttablecolumn: "CONCEPT_CD", c_comment: null, c_totalnum: null]
+        ]
+        println(obj[0])
+        def actual = TumorOnt.build(obj)
+        println(actual)
+    }
+
+    void testOnt() {
+        def actual = TumorOnt.NAACCR_I2B2.ont_view_in("task123", LocalDate.of(2000, 1, 1), Paths.get(cache))
+        for (Object x: actual) {
+            println(x)
+        }
     }
 }
