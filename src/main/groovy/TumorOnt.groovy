1c04388e851026d21030ce5ed675e2f99e0ebcfb
Dan Connolly <dconnolly@kumc.edu>
2020-03-04 11:52:49 -0600

    NAACCR_I2B2.ont_script - port find_ddl etc. to groovy

--- src/main/groovy/TumorOnt.groovy
+++ src/main/groovy/TumorOnt.groovy
@@ -4,7 +4,9 @@ import tech.tablesaw.columns.Column
 import tech.tablesaw.io.csv.CsvReadOptions
 
 import java.nio.charset.Charset
+import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.Paths
 import java.time.LocalDate
 import java.util.zip.ZipFile
 
@@ -16,7 +18,8 @@ class TumorOnt {
         static String encoding = 'ISO-8859-1'
 
         static Table read_table(Path cache, Tuple info) {
-            String zip = info[0] as String; String item = info[1] as String; List<String> names = info[2] as List<String>
+            String zip = info[0] as String; String item = info[1] as String
+            List<String> names = info[2] as List<String>
             def archive = new ZipFile(cache.resolve(zip).toFile())
             def infp = new InputStreamReader(archive.getInputStream(archive.getEntry(item)), Charset.forName(encoding))
             CsvReadOptions options = CsvReadOptions.builder(infp)
@@ -37,29 +40,78 @@ class TumorOnt {
         result
     }
 
-    static String top_folder = "\\i2b2\\naaccr\\"
-    static String c_name = 'Cancer Cases (NAACCR Hierarchy)'
-    static String sourcesystem_cd = 'heron-admin@kumc.edu'
+    static class SqlScript {
+        final String name
+        final String code
+        final List<Tuple3<String, String, List<String>>> objects
+
+        SqlScript(String _name, String _code, List<Tuple2<String, List<String>>> object_info) {
+            name = _name
+            code = _code
+            objects = object_info.collect { new Tuple3(it.first, find_ddl(it.first, code), it.second) }
+        }
+
+        static String find_ddl(String name, String script) {
+            String comment_pattern = '((--[^\\n]*(?:\\n|$))|(?:/\\*(?:[^*]|(\\*(?!/)))*\\*/))*'
+            Scanner stmts = new Scanner(script).useDelimiter(';\n\\s*' + comment_pattern)
+            while (stmts.hasNext()) {
+                String stmt = stmts.next().trim()
+                if (stmt.startsWith('create ')) {
+                    String firstLine = stmt.split('\n')[0]
+                    if (firstLine.split().contains(name)) {
+                        return stmt
+                    }
+                }
+            }
+            throw new IllegalArgumentException(name)
+        }
+    }
+
     static class NAACCR_I2B2 {
+        static final String top_folder = "\\i2b2\\naaccr\\"
+        static final String c_name = 'Cancer Cases (NAACCR Hierarchy)'
+        static final String sourcesystem_cd = 'heron-admin@kumc.edu'
+
+        static final String per_item_view = 'tumor_item_type'
+        static SqlScript ont_script = new SqlScript(
+                'naaccr_concepts_load.sql',
+                resourceText(NAACCR_I2B2.getResource('heron_load/naaccr_concepts_load.sql')),
+                [
+                        new Tuple2('i2b2_path_concept', []),
+                        new Tuple2('naaccr_top_concept', ['naaccr_top', 'current_task']),
+                        new Tuple2('section_concepts', ['section', 'naaccr_top']),
+                        new Tuple2('item_concepts', [per_item_view]),
+                        new Tuple2('code_concepts', [per_item_view, 'loinc_naaccr_answer', 'code_labels']),
+                        new Tuple2('primary_site_concepts', ['icd_o_topo']),
+                        // TODO: morphology
+                        new Tuple2('seer_recode_concepts', ['seer_site_terms', 'naaccr_top']),
+                        new Tuple2('site_schema_concepts', ['cs_terms']),
+                        new Tuple2('naaccr_ontology', []),
+                ])
+
         static def ont_view_in(String task_hash, LocalDate update_date, Path who_cache) {
             // TODO: make who_cache optional
             def info = OncologyMeta.topo_info
             def who_topo = OncologyMeta.read_table(who_cache, info)
             // TODO info = OncologyMeta.icd_o_info
             // def icd_o_topo = OncologyMeta.read_table(who_cache, info.zip, info.items, info.names)
-            Table top = build([[c_hlevel :1,
-                                c_fullname: top_folder,
-                                c_name: c_name,
-                                update_date: update_date,
+            Table top = build([[c_hlevel       : 1,
+                                c_fullname     : top_folder,
+                                c_name         : c_name,
+                                update_date    : update_date,
                                 sourcesystem_cd: sourcesystem_cd] as Map])
             def current_task = build([[task_hash: task_hash] as Map]).setName("current_task")
             [who_topo, top, current_task]
         }
     }
 
+    static String resourceText(URL url) {
+        new String(Files.readAllBytes(Paths.get(url.toURI())))
+    }
+
     static Table build(List<Map<String, Object>> obj) {
-        Collection<Column<?>> cols = (obj[0].collect { k, v ->
-            switch(v) {
+        Collection<Column<?>> cols = ((obj[0].collect { k, v ->
+            switch (v) {
                 case String:
                     return StringColumn.create(k, v as String)
                 case Integer:
@@ -69,12 +121,12 @@ class TumorOnt {
                 default:
                     throw new IllegalArgumentException("Expected String or Int in 1st record, not:" + v)
             }
-        }) as Collection<Column<?>>
+        }) as Collection<Column<?>>)
         Table data = Table.create("t1", cols)
         obj.tail().each { Map<String, Object> m ->
             Row row = data.appendRow()
             for (it in m) {
-                switch(it.value) {
+                switch (it.value) {
                     case String:
                         row.setString(it.key, it.value as String)
                         break
--- src/test/groovy/TumorOntTest.groovy
+++ src/test/groovy/TumorOntTest.groovy
@@ -3,39 +3,46 @@ import junit.framework.TestCase
 import java.nio.file.Paths
 import java.time.LocalDate
 
-class TumorOntTest extends TestCase  {
-    static def cache = ',cache/'
+class TumorOntTest extends TestCase {
+    static final cache = ',cache/'
 
     void testOncologyMeta() {
-        def info = TumorOnt.OncologyMeta.morph3_info
-        def morph = TumorOnt.OncologyMeta.read_table(Paths.get(cache), info)
+        final meta = TumorOnt.OncologyMeta
+        final morph = meta.read_table(Paths.get(cache), meta.morph3_info)
         assert morph.columnNames() == ["code", "label", "notes"]
 
         // test encoding
-        def non_ascii = morph.where(morph.stringColumn("code").isEqualTo("M8950/3"))
+        final non_ascii = morph.where(morph.stringColumn("code").isEqualTo("M8950/3"))
         println(non_ascii)
         assert non_ascii.get(0, 1) == "M\u009Fllerian mixed tumour"
 
-        info = TumorOnt.OncologyMeta.topo_info
-        def topo = TumorOnt.OncologyMeta.read_table(Paths.get(cache), info)
+        final topo = meta.read_table(Paths.get(cache), meta.topo_info)
         println(topo.first(5))
         assert topo.columnNames() == ["Kode", "Lvl", "Title"]
     }
 
-
     void testTableExpr() {
-        def obj = [
+        final obj = [
                 [c_facttablecolumn: "x", c_comment: "xx", c_totalnum: 1],
                 [c_facttablecolumn: "CONCEPT_CD", c_comment: null, c_totalnum: null]
         ]
         println(obj[0])
-        def actual = TumorOnt.build(obj)
+        final actual = TumorOnt.build(obj)
         println(actual)
     }
 
+    void testSqlScript() {
+        // TODO: commit symlink from src/main/resources to heron_load
+        URL url = getClass().getResource('heron_load/naaccr_concepts_load.sql')
+        String sql = TumorOnt.resourceText(url)
+        assert sql.indexOf('select') > 0
+        final script = TumorOnt.NAACCR_I2B2.ont_script
+        assert script.objects.last().first == 'naaccr_ontology'
+    }
+
     void testOnt() {
-        def actual = TumorOnt.NAACCR_I2B2.ont_view_in("task123", LocalDate.of(2000, 1, 1), Paths.get(cache))
-        for (Object x: actual) {
+        final actual = TumorOnt.NAACCR_I2B2.ont_view_in("task123", LocalDate.of(2000, 1, 1), Paths.get(cache))
+        for (Object x : actual) {
             println(x)
         }
     }
